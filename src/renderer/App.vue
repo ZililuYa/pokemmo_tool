630d9d8075c691a1aef6dc8e4ff19b1e2868a695
鲁康 <lukang@co-mall.com>
2020-03-10 15:23:18 +0800

    no message

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PokeMMO最全工具'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
