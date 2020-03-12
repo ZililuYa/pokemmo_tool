e72a979b5ca23c1d6788e1d3f1d78faf4ea0261d
鲁康 <lukang@co-mall.com>
2020-03-12 12:12:29 +0800

    no message

--- src/renderer/components/Pvp.vue
+++ src/renderer/components/Pvp.vue
@@ -1,8 +1,58 @@
 <template>
-    <md-content class="md-layout">
+    <md-content class="md-layout md-gutter">
         <div class="md-layout-item md-size-50">
             <div class="text-center">
                 <md-button class="md-raised md-primary" @click="showDialog=true">进攻宝可梦</md-button>
+            </div>
+
+            <div v-if="attackPm">
+                <div class="text-center">
+                    <div class="sprite-icon" :class="'sprite-icon-'+attackPm.index"></div>
+                </div>
+                <div class="md-layout md-gutter">
+                    <div class="md-layout-item md-size-50">
+                        <md-field>
+                            <label>宝可梦名称</label>
+                            <md-input v-model="attackPm.nameZh" :disabled="true"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-size-50">
+                        <md-field md-clearable>
+                            <label>等级</label>
+                            <md-input v-model="attackPm.level" type="number"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-size-50">
+                        <div class="radio">
+                            <md-radio v-model="attackPm.ability" v-if="attackPm.ability1" :value="attackPm.ability1">
+                                {{attackPm.ability1}}
+                            </md-radio>
+                            <md-radio v-model="attackPm.ability" v-if="attackPm.ability2" :value="attackPm.ability2">
+                                {{attackPm.ability2}}
+                            </md-radio>
+                            <md-radio v-model="attackPm.ability" v-if="attackPm.abilityHide"
+                                      :value="attackPm.abilityHide">
+                                {{attackPm.abilityHide}}
+                            </md-radio>
+                        </div>
+                    </div>
+                    <div class="md-layout-item md-size-50">
+                        <div class="radio">
+                            <md-radio v-model="attackPm.zt" :value="0">
+                                无
+                            </md-radio>
+                            <md-radio v-model="attackPm.zt" :value="1">
+                                灼伤
+                            </md-radio>
+                            <md-radio v-model="attackPm.zt" :value="2">
+                                麻痹
+                            </md-radio>
+                        </div>
+                    </div>
+                </div>
+
+                <BaseStat :data="attackPm.baseStat"></BaseStat>
+
             </div>
         </div>
         <div class="md-layout-item md-size-50">
@@ -11,9 +61,32 @@
             </div>
         </div>
         <div class="md-layout-item md-size-100">
-            <div class="text-center">
-                <md-button class="md-raised md-accent">战斗计算</md-button>
-            </div>
+            <!--            <div class="text-center">-->
+            <!--                <md-button class="md-raised md-accent">战斗计算</md-button>-->
+            <!--            </div>-->
+            <md-table>
+                <md-table-row>
+                    <!--                    <md-table-head md-numeric>ID</md-table-head>-->
+                    <md-table-head>招式名称</md-table-head>
+                    <md-table-head :width=100>属性</md-table-head>
+                    <md-table-head :width=100>分类</md-table-head>
+                    <md-table-head>威力</md-table-head>
+                    <md-table-head>伤害</md-table-head>
+                    <md-table-head>几次击杀</md-table-head>
+                </md-table-row>
+
+                <md-table-row v-for="(i,k) in learnSet">
+                    <!--                    <md-table-cell md-numeric>{{k}}</md-table-cell>-->
+                    <md-table-cell>{{i.move}}</md-table-cell>
+                    <md-table-cell :class="'category'+i.category">{{i.category}}</md-table-cell>
+                    <md-table-cell :class="'type'+i.type">{{i.type}}</md-table-cell>
+                    <md-table-cell>{{i.power}}</md-table-cell>
+                    <md-table-cell>{{damageCalc(i.power)}}</md-table-cell>
+                    <md-table-cell>2次</md-table-cell>
+                </md-table-row>
+
+            </md-table>
+
         </div>
 
         <md-dialog :md-active.sync="showDialog" :style="{width: '80%'}">
@@ -27,7 +100,9 @@
                     <div class="sprite-icon"
                          v-if="!(pm && JSON.stringify(i).indexOf(pm)===-1)"
                          :class="'sprite-icon-'+i.index"
+                         @click="clickPoke(i.index)"
                          v-for="i in pmList">
+                        <md-tooltip md-direction="top">{{i.nameZh}}</md-tooltip>
                     </div>
                 </div>
             </div>
@@ -41,19 +116,70 @@
 
 <script>
 
+    import BaseStat from './component/BaseStat'
+
     export default {
         data() {
             return {
                 showDialog: false,
                 pm: '',
+                attackPm: undefined,
+                learnSet: [],
                 pmList: [],
             }
         },
         name: 'home',
-        components: {},
-        methods: {},
+        components: {BaseStat},
+        methods: {
+            damageCalc(i) {
+                return i + ' - 200'
+            },
+            categoryCalc(list, item, name) {
+                if ((item.category === '物理' || item.category === '特殊') && item.type !== '妖精' && !isNaN(item.power) && name.indexOf(item.move) === -1)
+                    list.push(item);
+                return list;
+            },
+            clickPoke(index) {
+                let len = parseInt(index);
+                this.$http({
+                    url: '/pokemon/detail', lsn: 'poke' + len, data: {index: len}, callback: (data) => {
+                        data = data[0];
+                        console.log(data);
+                        data.level = 50;
+                        data.index = index;
+                        data.ability = data.ability1;
+                        let nl = Object.keys(data.baseStat), baseStat = data.baseStat;
+                        nl.forEach((i) => {
+                            baseStat[i + 'Nl'] = 252;
+                        });
+                        data.baseStat = baseStat;
+                        data.zt = 0;
+                        // learnSetByBreeding learnSetByLevelingUp learnSetByTechnicalMachine
+                        let learnSet = [], learnName = [];
+                        if (data.learnSetByBreeding) data.learnSetByBreeding.forEach((item) => {
+                            learnSet = this.categoryCalc(learnSet, item, learnName);
+                            learnName.push(item.move);
+                        });
+                        if (data.learnSetByLevelingUp) data.learnSetByLevelingUp.forEach((item) => {
+                            learnSet = this.categoryCalc(learnSet, item, learnName);
+                            learnName.push(item.move);
+                        });
+                        if (data.learnSetByTechnicalMachine) data.learnSetByTechnicalMachine.forEach((item) => {
+                            learnSet = this.categoryCalc(learnSet, item, learnName);
+                            learnName.push(item.move);
+                        });
+                        console.log(learnSet);
+                        this.learnSet = learnSet;
+                        this.attackPm = data;
+                        // this.$set(this.attackPm, data);
+                        this.showDialog = false;
+                    }
+                })
+            }
+        },
         mounted() {
             // console.log(this.$http)
+            this.clickPoke('006');
             this.$http({
                 url: '/pokemon/list', lsn: 's1', data: {generation: 1}, callback: (data) => {
                     console.log(data);
@@ -97,11 +223,105 @@
 
     .md-layout {
         .md-size-50 {
-            background-color: #f9f9f9;
         }
 
         .text-center {
             text-align: center;
+        }
+
+        .category特殊 {
+            background-color: #2266CC !important;
+            color: #BBEEFF !important;
+        }
+
+        .category物理 {
+            background-color: #FF4400 !important;
+            color: #FFCC00 !important;
+        }
+
+        .type一般 {
+            background-color: #BBBBAA !important;
+            color: #FFF !important;
+        }
+
+        .type火 {
+            background-color: #FF4422 !important;
+            color: #FFF !important;
+        }
+
+        .type水 {
+            background-color: #3399FF !important;
+            color: #FFF !important;
+        }
+
+        .type电 {
+            background-color: #FFCC33 !important;
+            color: #FFF !important;
+        }
+
+        .type草 {
+            background-color: #77CC55 !important;
+            color: #FFF !important;
+        }
+
+        .type毒 {
+            background-color: #AA5599 !important;
+            color: #FFF !important;
+        }
+
+        .type冰 {
+            background-color: #77DDFF !important;
+            color: #FFF !important;
+        }
+
+        .type格斗 {
+            background-color: #BB5544 !important;
+            color: #FFF !important;
+        }
+
+        .type地面 {
+            background-color: #DDBB55 !important;
+            color: #FFF !important;
+        }
+
+        .type飞行 {
+            background-color: #6699FF !important;
+            color: #FFF !important;
+        }
+
+        .type超能力 {
+            background-color: #FF5599 !important;
+            color: #FFF !important;
+        }
+
+        .type虫 {
+            background-color: #AABB22 !important;
+            color: #FFF !important;
+        }
+
+        .type岩石 {
+            background-color: #BBAA66 !important;
+            color: #FFF !important;
+        }
+
+        .type幽灵 {
+            background-color: #6666BB !important;
+            color: #FFF !important;
+        }
+
+        .type恶 {
+            background-color: #775544 !important;
+            color: #FFF !important;
+        }
+
+        .type龙 {
+            background-color: #7766EE !important;
+            color: #FFF !important;
+        }
+
+        .type钢 {
+            background-color: #AAAABB !important;
+            color: #FFF !important;
         }
     }
 
---
+++ src/renderer/components/component/BaseStat.vue
@@ -0,0 +1,190 @@
+<template>
+    <div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">HP</label>
+                    <md-input v-model="baseStat.hp" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.hpNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.hp" :disabled="true"/>
+                </md-field>
+
+
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">攻击</label>
+                    <md-input v-model="baseStat.attack" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.attackNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.attack" :disabled="true"/>
+                </md-field>
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">防御</label>
+                    <md-input v-model="baseStat.defense" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.defenseNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.defense" :disabled="true"/>
+                </md-field>
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">特攻</label>
+                    <md-input v-model="baseStat.spAttack" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.spAttackNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.spAttack" :disabled="true"/>
+                </md-field>
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">特防</label>
+                    <md-input v-model="baseStat.spDefense" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.spDefenseNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.spDefense" :disabled="true"/>
+                </md-field>
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+        <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="first-name">速度</label>
+                    <md-input v-model="baseStat.speed" :disabled="true" type="number"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field md-clearable>
+                    <label for="last-name">努力值</label>
+                    <md-input v-model="baseStat.speedNl"/>
+                </md-field>
+            </div>
+
+            <div class="md-layout-item md-size-20">
+                <md-field>
+                    <label for="last-name">能力值</label>
+                    <md-input v-model="baseStat.speed" :disabled="true"/>
+                </md-field>
+            </div>
+            <div class="md-layout-item md-size-40">
+                <md-checkbox v-model="baseStat.hpXg" value="1.1">加</md-checkbox>
+                <md-checkbox v-model="baseStat.hpXg" value="0.9">减</md-checkbox>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                baseStat: {}
+            }
+        },
+        props: ['data'],
+        methods: {
+            init(data) {
+                this.baseStat = data;
+            }
+        },
+        mounted() {
+            this.init(this.$props.data);
+        },
+        watch: {
+            data(data) {
+                this.init(data);
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+</style>
